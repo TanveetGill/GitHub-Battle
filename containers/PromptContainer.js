f291af23b046301c8a68d73f1b0dfa5bbe2ff1eb
TanveetGill <tanveet.gill@gmail.com>
2016-07-15 11:56:14 -0400

    seperated UI from Logic in Components

--- containers/PromptContainer.js
+++ containers/PromptContainer.js
@@ -1,5 +1,5 @@
 var React = require('react');
-var transparentBg = require('../styles/index').transparentBg;
+var Prompt = require('../components/Prompt');
 
 var PromptContainer = React.createClass({
   contextTypes: {
@@ -10,12 +10,12 @@ var PromptContainer = React.createClass({
       username: ''
     }
   },
-  onUpdateUser: function (e) {
+  handleUpdateUser: function (e) {
     this.setState({
       username: e.target.value
     })
   },
-  onSubmitUser: function (e) {
+  handleSubmitUser: function (e) {
     e.preventDefault();
     var username = this.state.username;
     this.setState({
@@ -23,33 +23,24 @@ var PromptContainer = React.createClass({
     });
 
     if (this.props.routeParams.playerOne) {
-      // goto /battle route
+      this.context.router.push({
+        pathname: '/battle',
+        query: {
+          playerOne: this.props.routeParams.playerOne,
+          playerTwo: this.state.username
+        }
+      })
     } else {
-      // goto /playertwo route
+      this.context.router.push('/playerTwo/' + this.state.username)
     }
   },
   render: function() {
     return (
-      <div className='jumbotron col-sm-6 col-sm-offset-3 text-center' style={transparentBg}>
-        <h1>{this.props.route.header}</h1>
-        <div className='col-sm-12'>
-          <form onSubmit={this.onSubmitUser}>
-            <div className='form-group'>
-              <input 
-                className='form-control' 
-                placeholder='Github Username' 
-                onChange={this.onUpdateUser} 
-                value={this.state.username}
-                type='text'/>
-            </div>
-            <div className='form-group col-sm-4 col-sm-offset-4'>
-              <button className='btn btn-block btn-success' type='submit'>
-                Continue
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+      <Prompt 
+        onSubmitUser={this.handleSubmitUser}
+        onUpdateUser={this.handleUpdateUser}
+        header={this.props.route.header}
+        username={this.state.username} />
     )
   }
 });
